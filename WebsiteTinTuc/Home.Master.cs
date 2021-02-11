2d9396354c527a96bd2cea5d9e2a4424672b50a3
cuong1998pro <cuong1998pro@gmail.com>
2021-02-11 18:27:30 +0700

    hien thi du lieu menu

---
+++ WebsiteTinTuc/Home.Master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebsiteTinTuc
+{
+    public partial class Home : System.Web.UI.MasterPage
+    {
+        private TinTucDataContext dt = new TinTucDataContext();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            rpBanTin.DataSource = dt.BanTin_SelectAll();
+            rpBanTin.DataBind();
+        }
+    }
+}
---
+++ WebsiteTinTuc/Index.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Home.Master" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebsiteTinTuc.Index" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>Tiêu đề nội dung chính</h1>
+    <div id="ndcontent">
+        Nội dung Website sẽ hiển thị ở đây
+    </div>
+</asp:Content>
