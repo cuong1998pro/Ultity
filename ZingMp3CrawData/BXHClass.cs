2f7c6ee9a36329c2065a026a1d9dfbd719e7763e
cuong1998pro <cuong1998pro@gmail.com>
2021-02-16 02:08:17 +0700

    craw bxh

---
+++ ZingMp3CrawData/BXHClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZingMp3CrawData
+{
+    public class Rootobject
+    {
+        public int err { get; set; }
+        public string msg { get; set; }
+        public Data data { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class Data
+    {
+        public string banner { get; set; }
+        public string playlistId { get; set; }
+        public int chartId { get; set; }
+        public string cover { get; set; }
+        public string country { get; set; }
+        public string type { get; set; }
+        public Group[] group { get; set; }
+        public string link { get; set; }
+        public int week { get; set; }
+        public int year { get; set; }
+        public int latestWeek { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public Item[] items { get; set; }
+        public string sectionId { get; set; }
+    }
+
+    public class Group
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+        public string link { get; set; }
+    }
+
+    public class Item
+    {
+        public string encodeId { get; set; }
+        public string title { get; set; }
+        public string alias { get; set; }
+        public bool isOffical { get; set; }
+        public string username { get; set; }
+        public string artistsNames { get; set; }
+        public Artist[] artists { get; set; }
+        public bool isWorldWide { get; set; }
+        public string thumbnailM { get; set; }
+        public string link { get; set; }
+        public string thumbnail { get; set; }
+        public int duration { get; set; }
+        public bool isZMA { get; set; }
+        public bool zingChoise { get; set; }
+        public bool isPrivate { get; set; }
+        public bool preRelease { get; set; }
+        public int releaseDate { get; set; }
+        public int streamingStatus { get; set; }
+        public bool allowAudioAds { get; set; }
+        public bool hasLyric { get; set; }
+        public int rakingStatus { get; set; }
+        public int score { get; set; }
+        public string mvlink { get; set; }
+    }
+
+    public class Artist
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string link { get; set; }
+        public bool spotlight { get; set; }
+    }
+}
--- ZingMp3CrawData/MainWindow.xaml.cs
+++ ZingMp3CrawData/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -36,6 +41,7 @@ namespace ZingMp3CrawData
                 isCheckVietNam = value;
                 isCheckAuMy = false;
                 isCheckHanQuoc = false;
+                lsbTopSong.ItemsSource = listVN;
                 OnPropertyChanged("IsCheckVietNam");
                 OnPropertyChanged("IsCheckAuMy");
                 OnPropertyChanged("IsCheckHanQuoc");
@@ -49,6 +55,7 @@ namespace ZingMp3CrawData
                 isCheckVietNam = false;
                 isCheckAuMy = value;
                 isCheckHanQuoc = false;
+                lsbTopSong.ItemsSource = listEU;
                 OnPropertyChanged("IsCheckVietNam");
                 OnPropertyChanged("IsCheckAuMy");
                 OnPropertyChanged("IsCheckHanQuoc");
@@ -62,6 +69,7 @@ namespace ZingMp3CrawData
                 isCheckVietNam = false;
                 isCheckAuMy = false;
                 isCheckHanQuoc = value;
+                lsbTopSong.ItemsSource = listKorean;
                 OnPropertyChanged("IsCheckVietNam");
                 OnPropertyChanged("IsCheckAuMy");
                 OnPropertyChanged("IsCheckHanQuoc");
@@ -70,16 +78,94 @@ namespace ZingMp3CrawData
 
         #endregion prop
 
+        private HttpClient http;
+
+        private Uri baseUrl = new Uri("https://zingmp3.vn/");
+
         public MainWindow()
         {
             InitializeComponent();
-            lsbTopSong.ItemsSource = new List<string>() { "", "", "" };
             this.DataContext = this;
-            IsCheckVietNam = true;
+            InitHttpClient();
+            CrawData(); IsCheckVietNam = true;
+
+            //ChonBXH(btnVietNam);
         }
 
-        private void ChonBXH(ToggleButton button)
+        private ObservableCollection<Item> listVN;
+        private ObservableCollection<Item> listEU;
+        private ObservableCollection<Item> listKorean;
+
+        private void InitHttpClient()
         {
+            HttpClientHandler handler = new HttpClientHandler();
+
+            CookieContainer cookie = new CookieContainer();
+            handler.CookieContainer = cookie;
+
+            handler.ClientCertificateOptions = ClientCertificateOption.Automatic;
+            handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            handler.AllowAutoRedirect = true;
+            handler.UseDefaultCredentials = false;
+
+            http = new HttpClient(handler);
+            http.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:86.0) Gecko/20100101 Firefox/86.0");
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "*/*");
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Encoding", "gzip, deflate, br");
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Accept-Language", "en-US");
+            http.DefaultRequestHeaders.TryAddWithoutValidation("Host", "zingmp3.vn");
+            http.BaseAddress = baseUrl;
+
+            //string cookieString = "zpsid=eMqpTcwdFagwSouHAhqaNBit8WujlofQlYib2pB8RYtq0HmR88bEEyr-0WutYKGCr4eLBJUbQ7_nI1nmPA9jQhnr2cPskLfoade2HqUCPMgXQMXSTU4wV0; zpsidleg=eMqpTcwdFagwSouHAhqaNBit8WujlofQlYib2pB8RYtq0HmR88bEEyr-0WutYKGCr4eLBJUbQ7_nI1nmPA9jQhnr2cPskLfoade2HqUCPMgXQMXSTU4wV0; __zi=3000.SSZzejyD0jSbZUgxWaGPoJIFlgNCIW6AQ9sqkju84vnyakgps4rUctwSxBQMIb-RCvlkiPzBKviqDG.1; _zlang=vn; zmp3_app_version.1=1022; zmp3_rqid=MHwxNC4yMzUdUngMjA4LjM5fHYxLjAdUngMjJ8MTYxMzM5MDExOTk5NA; zmp3_sid=mVkC9An3SndU_8uggaPl7jlZ-MxT34Kdo_6uJiirEZdUvhX4_2mEBltcb6YF2GmdeBMJRPf4CJ6sfuXQWce-EPUMlMMQMIrJZAJGEem1PLA_kVKaQMG; _ga=GA1.2.2139146386.1613375782; _gid=GA1.2.2084430700.1613375782; _fbp=fb.1.1613375781772.130300411; fpsend=149387";
+            //AddCookie(cookie, cookieString);
+        }
+
+        public int GetWeekNumber(DateTime date)
+        {
+            CultureInfo ciCurr = CultureInfo.CurrentCulture;
+            int weekNum = ciCurr.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return weekNum;
+        }
+
+        private string GetStringFromURL(string url)
+        {
+            return WebUtility.HtmlDecode(http.GetStringAsync(url).Result);
+        }
+
+        private void AddCookie(CookieContainer cookieContainer, string cookies)
+        {
+            var cookieArray = cookies.Split(';');
+            foreach (var cookie in cookieArray)
+            {
+                var result = cookie.Split('=');
+                if (result.Count() == 2)
+                {
+                    string key = result[0].ToString().Trim();
+                    string value = result[1].ToString().Trim();
+                    cookieContainer.Add(baseUrl, new Cookie(key, value));
+                }
+            }
+        }
+
+        private void CrawData()
+        {
+            var week = GetWeekNumber(DateTime.Now) - 2;
+            var year = DateTime.Now.Year;
+
+            http.GetStringAsync("https://zingmp3.vn/zing-chart-tuan/bai-hat-Kpop/IWZ9Z0BO.html");
+
+            //var tick = DateTime.Now.Subtract(
+            //    new DateTime(1970, 1, 1)
+            //    ).TotalSeconds;
+
+            var urlBXHViet = $"https://zingmp3.vn/api/v2/chart/getWeekChart?id=IWZ9Z08I&week={week}&year={year}&ctime=1613376233&version=1.0.22&sig=d83daaaa21bf6b8f95b880abee644e901c5f4e53264b03fc789402d426b51f14a9d965fbeb9249c861bd55e165d183ca23a8e1f2d6c7e5e95d6e7c62e5796a6a&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw";
+            var urlBXHAuMy = $"https://zingmp3.vn/api/v2/chart/getWeekChart?id=IWZ9Z0BW&week={week}&year={year}&ctime=1613376338&version=1.0.22&sig=556d327b701f0290f1515915310baec9e3b790e951c420b20ef2eb39a6ead25215688f12ea328a911e6f0d074688392d8639e142b091d0d72e21ea133fb8908d&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw";
+            var urlBXHHanQuoc = $"https://zingmp3.vn/api/v2/chart/getWeekChart?id=IWZ9Z0BO&week={week}&year={year}&ctime=1613376340&version=1.0.22&sig=f500e034cf46b696eb62e84afaa6c4fea242ab971bff4fc0cbe06c4ce54f31e8d04f48166e308100cd8cf28a761a5ea86e36e5421732c434b6749c8e71b90559&apiKey=kI44ARvPwaqL7v0KuDSM0rGORtdY1nnw";
+
+            var str = GetStringFromURL(urlBXHViet);
+            listVN = new ObservableCollection<Item>(JsonConvert.DeserializeObject<Rootobject>(GetStringFromURL(urlBXHViet)).data.items);
+            listEU = new ObservableCollection<Item>(JsonConvert.DeserializeObject<Rootobject>(GetStringFromURL(urlBXHAuMy)).data.items);
+            listKorean = new ObservableCollection<Item>(JsonConvert.DeserializeObject<Rootobject>(GetStringFromURL(urlBXHHanQuoc)).data.items);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -91,7 +177,5 @@ namespace ZingMp3CrawData
                 PropertyChanged(this, new PropertyChangedEventArgs(newName));
             }
         }
-
-       
     }
 }
